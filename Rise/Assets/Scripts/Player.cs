1a95dffd80bd607868900ffbfb7e9d11ce247d92
xhenoby <55144300+xhenoby@users.noreply.github.com>
2020-05-23 21:23:35 -0300

    movimiento

---
+++ Rise/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool changeGravity,grounded;
+    private Rigidbody2D rb2D;
+    public float gravity;
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        rb2D.velocity = new Vector2(1f, 0);
+    }
+    void Update()
+    {
+        rb2D.velocity = new Vector2(gravity, 0);
+        if (Input.GetMouseButtonDown(0) && grounded)
+        {
+            gravity=gravity*-1;
+        }
+    }
+}
