9bdde08f691148d476a89233a01aa7b21bd61699
Nicolas <nicolasmanz@gmail.com>
2020-05-23 23:36:29 -0300

    Simple improvements to the project overall along with movement issues.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool changeGravity,grounded;
+    private Rigidbody2D rb2D;
+    public float gravity;
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        rb2D.velocity = new Vector2(1f, 0);
+    }
+    void Update()
+    {
+        rb2D.velocity = new Vector2(gravity, 0);
+        if (Input.GetMouseButtonDown(0) && grounded)
+        {
+            grounded = false;
+            gravity *= -1;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("Wall"))
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Object")
+        {
+            Debug.Log("Object hit");
+        }
+    }
+}
