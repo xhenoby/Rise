8db6dbc4ac1b894d0e0bf027c1ab7322c9040677
xhenoby <55144300+xhenoby@users.noreply.github.com>
2020-05-28 02:56:10 -0300

    Menu

---
+++ Assets/ControladorDeUI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class ControladorDeUI : MonoBehaviour
+{
+    public GameObject MenuInicio, MenuGameover,Boton,Replay;
+    public bool replay;
+
+    void Start()
+    {
+        Replay = GameObject.FindGameObjectWithTag("Replay");
+        replay = Replay.GetComponent<Replay>().Rep;
+        if (replay)
+        {
+            MenuGameover.SetActive(false);
+            MenuInicio.SetActive(false);
+            Boton.SetActive(true);
+        }
+        if (!replay)
+        {
+            MenuInicio.SetActive(true);
+            Boton.SetActive(false);
+            MenuGameover.SetActive(false);
+            
+        }
+        replay = true;
+    }
+    public void Play()
+    {
+        MenuInicio.SetActive(false);
+        Boton.SetActive(true);
+
+    }
+    public void Reset()
+    {
+        Replay = GameObject.FindGameObjectWithTag("Replay");
+        replay = Replay.GetComponent<Replay>().Rep;
+        replay = true;
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+
+    }
+    public void Exit()
+    {
+        Replay = GameObject.FindGameObjectWithTag("Replay");
+        Destroy(Replay);
+        Application.Quit();
+    }
+}
---
+++ Assets/Replay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Replay : MonoBehaviour
+{
+    public bool Rep;
+    public static Replay replay;
+    void Awake()
+    {
+        if (replay == null)
+        {
+            replay = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        if (replay!=this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rep = true;
+    }
+}
