7d7ebce8575c51e2e0e9835c6dce68133541ae7d
xhenoby <55144300+xhenoby@users.noreply.github.com>
2020-05-28 16:15:18 -0300

    Audio y Menus

--- Assets/ControladorDeUI.cs
+++ Assets/ControladorDeUI.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.SceneManagement;
-using UnityEngine.SceneManagement;
 
 public class ControladorDeUI : MonoBehaviour
 {
-    public GameObject MenuInicio, MenuGameover,Boton,Replay;
+    public GameObject MenuInicio, MenuGameover,Boton,Replay,black;
     public bool replay;
+    public float TiempoTransicion;
+    public AudioSource Inicio, GameOver, Playing;
+
+    public float TiempoTrancurido;
+    public bool transicion1, transicion2;
 
     void Start()
     {
@@ -18,30 +21,55 @@ public class ControladorDeUI : MonoBehaviour
             MenuGameover.SetActive(false);
             MenuInicio.SetActive(false);
             Boton.SetActive(true);
+            Playing.Play();
+            Playing.volume = 0;
+            transicion1 = true;
         }
         if (!replay)
         {
             MenuInicio.SetActive(true);
             Boton.SetActive(false);
             MenuGameover.SetActive(false);
-            
+            Inicio.Play();
+
         }
-        replay = true;
+    }
+    private void Update()
+    {
+        if (transicion1)
+        {
+            TiempoTrancurido += (Time.deltaTime/TiempoTransicion);
+            Inicio.volume = Mathf.Lerp(1,0,TiempoTrancurido);
+            Playing.volume = Mathf.Lerp(0,1,TiempoTrancurido);
+        }
+        if (transicion2)
+        {
+            TiempoTrancurido += (Time.deltaTime/TiempoTransicion);
+            GameOver.volume = Mathf.Lerp(0, 1,TiempoTrancurido);
+            Playing.volume = Mathf.Lerp(1, 0,TiempoTrancurido);
+        }
+        if (TiempoTrancurido > 1)
+        {
+            transicion1 = false;
+            transicion2 = false;
+        }
     }
     public void Play()
     {
         MenuInicio.SetActive(false);
         Boton.SetActive(true);
-
+        TiempoTrancurido = 0;
+        transicion1 = true;
+        Playing.Play();
+        Playing.volume = 0;
     }
-    public void Reset()
+    public void Gameover()
     {
-        Replay = GameObject.FindGameObjectWithTag("Replay");
-        replay = Replay.GetComponent<Replay>().Rep;
-        replay = true;
-        Scene scene = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(scene.name);
-
+        MenuGameover.SetActive(true);
+        GameOver.Play();
+        GameOver.volume = 0;
+        TiempoTrancurido = 0;
+        transicion2 = true;
     }
     public void Exit()
     {
